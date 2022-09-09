40b8844c8f7ece8981ae53364e7f97ad0495d36e
Michael Bernay <mb246721@ohio.edu>
2022-09-09 18:08:25 -0400

    Git Lab Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Michael Bernay>
+ *        Date: <Friday, September 9, 2022>
+ * Description: git lab program for lab2 CS2400
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
